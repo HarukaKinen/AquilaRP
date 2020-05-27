8efa36812a91a403744a03caeaae34082982f324
Kotoki1337 <Kotoki1337@gmail.com>
2020-05-28 00:06:48 +0800

    Add icon

--- GUI.py
+++ GUI.py
@@ -8,6 +8,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon
 import sys
 
 
@@ -15,6 +16,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(359, 212)
+        MainWindow.setWindowIcon(QIcon("AQN.ico"))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
